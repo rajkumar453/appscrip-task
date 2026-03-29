fa4655cb53ab3c35671033ec56c2e742d899c735
rajkumar453 <pinikesunny@gmail.com>
2026-03-29 17:47:10 +0530

    Remove next.config.js - project uses CRA not Next.js

--- next.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('next').NextConfig} */
-// Only needed if using Next.js instead of Create React App
-// Add "use client"; as first line of App.js and rename to src/app/page.js
-const nextConfig = { reactStrictMode: true };
-module.exports = nextConfig;
